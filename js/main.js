63ed115379e18250e412e952e2c436b608179395
Jodory <jodory9524@gmail.com>
2022-03-28 22:07:25 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-const searchEl = document.querySelector('.search');
-// .search클래스 내부에 input div
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function() {
-    //logic..
-    //search가 클릭되면 포커스 되라
-    //.search클래스 내부엔 input도 있고 icon도 있다.
-    searchInputEl.focus();
-});
-
-//search클래스 내부에 input이 포커스 되면
-// setAttribute(속성삽입)을 통해 통합검색 삽입by placeholder
-searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-// blur == 포커스 아웃 // 되면 
-// 속성을 삭제한다
-searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector('#to-top');
 
@@ -169,6 +143,3 @@ spyEls.forEach(function(spyEl) {
         triggerHook: .8 //요소 검사할 시작범위 지정
     }).setClassToggle(spyEl, 'show').addTo(new ScrollMagic.Controller());
 })
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
